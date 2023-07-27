c5e5f2d114a419429325ce6eeb98dbfa7f6deb50
unknown <55wokqe9@anonaddy.me>
2023-07-27 14:40:34 +0100

    Complete stage 1

--- Src/main.py
+++ Src/main.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from itertools import combinations
 from typing import TYPE_CHECKING
 
 import ase
@@ -41,9 +42,47 @@ def initial_positioning(reactant: ase.Atoms,
                         reactant_molecules: list[ase.Atoms],
                         product_molecules: list[ase.Atoms],
                         reactivity_matrix: dok_matrix):
-    # Translate reactant molecules so that their geometric centres are at origin
-    for i, reactant_mol in enumerate(reactant_molecules):
-        reactant_mol.translate(np.mean(reactant_mol.get_positions(), axis=0))
+    coordinates = reactant.get_positions()
+
+    shared_atom_geometric_centres = []
+    reactive_atom_geometric_centres = []
+
+    for reactant_mol in reactant_molecules:
+        # Translate reactant molecules so that their geometric centres are (approximately) at origin
+        reactant_mol.translate(-np.mean(reactant_mol.get_positions(), axis=0))
+
+        # Calculate geometric centres of reactive (B) and shared atoms (C)
+        for product_mol in product_molecules:
+            shared = get_shared_atoms(reactant_mol, product_mol)
+            reactive = get_reactive_atoms(shared, reactivity_matrix)
+
+            if shared.size > 0:
+                shared_atom_geometric_centres.append(np.mean(coordinates[shared, :], axis=0))
+            if reactive.size > 0:
+                reactive_atom_geometric_centres.append(np.mean(coordinates[reactive, :], axis=0))
+
+    # Calculate the geometric centres of bond-forming atoms (A)
+    bonding_atom_geometric_centres = []
+    for rmol1, rmol2 in combinations(reactant_molecules, 2):
+        atoms = get_bond_forming_atoms(rmol1, rmol2, True, reactivity_matrix)
+        bonding_atom_geometric_centres.append(np.mean(coordinates[atoms, :], axis=0))
+
+    # Calculate the vectors which will be used to reposition the reactants and products
+    n_reactants = len(reactant_molecules)
+    beta = np.sum(np.array(reactive_atom_geometric_centres), axis=0) / n_reactants
+    sigma = np.sum(np.array(shared_atom_geometric_centres), axis=0) / n_reactants
+
+    reactant_repositioning_vector = np.sum(np.array(bonding_atom_geometric_centres), axis=0) / n_reactants
+    product_repositioning_vector = (beta + sigma) / 2
+
+    # Translate all reactants and products using the computed vectors
+    reactant.translate(reactant_repositioning_vector)
+    for reactant_mol in reactant_molecules:
+        reactant_mol.translate(reactant_repositioning_vector)
+
+    product.translate(product_repositioning_vector)
+    for product_mol in product_molecules:
+        product_mol.translate(product_repositioning_vector)
 
 
 def get_bond_forming_atoms(molecule1: ase.Atoms,
@@ -65,11 +104,8 @@ def get_bond_forming_atoms(molecule1: ase.Atoms,
     return np.array(bonding_atoms)
 
 
-def get_reactive_atoms(reactant_molecule: ase.Atoms,
-                       product_molecule: ase.Atoms,
+def get_reactive_atoms(shared_atoms: np.ndarray,
                        reactivity_matrix: dok_matrix) -> np.ndarray:
-    shared_atoms = get_shared_atoms(reactant_molecule, product_molecule)
-
     reactive_atoms = []
     for atom in shared_atoms:
         row = reactivity_matrix[atom, :]
--- Tests/test_main.py
+++ Tests/test_main.py
@@ -47,6 +47,48 @@ def ester_hydrolysis_reaction():
     return reactant, product
 
 
+def test_initial_positioning(ester_hydrolysis_reaction):
+    reactant, product = ester_hydrolysis_reaction
+
+    reactant_molecules = separate_molecules(reactant)
+    product_molecules = separate_molecules(product)
+
+    reactivity_matrix = get_reactivity_matrix(reactant, product)
+
+    initial_positioning(reactant, product, reactant_molecules, product_molecules, reactivity_matrix)
+
+    reactant_expected = np.array([[-1.05141, 5.188985, 4.03545],
+                                  [-0.72742, 4.160835, 4.20875],
+                                  [-0.2049, 5.845275, 2.95923],
+                                  [-2.09973, 5.167505, 3.72872],
+                                  [-0.98183, 5.746465, 4.97171],
+                                  [-0.7025, 6.289725, 1.94582],
+                                  [1.13092, 5.932295, 3.18482],
+                                  [1.76924, 5.410365, 4.37711],
+                                  [2.84113, 5.615555, 4.31415],
+                                  [1.3614, 5.903955, 5.26329],
+                                  [1.61413, 4.329825, 4.43672],
+                                  [-0.99627, 1.395835, 1.36496],
+                                  [-0.87429, 1.441465, 2.30599]])
+
+    product_expected = np.array([[-4.03730858, 2.81617558, 2.27066408],
+                                 [-3.69396858, 1.78991558, 2.41853408],
+                                 [-3.15220858, 3.52586558, 1.27145408],
+                                 [-5.07007858, 2.79112558, 1.91441408],
+                                 [-4.01462858, 3.33248558, 3.23312408],
+                                 [-3.54348858, 3.92694558, 0.19546408],
+                                 [2.11144142, 4.86205558, 4.05148408],
+                                 [0.88128142, 4.97962558, 4.70576408],
+                                 [0.54706142, 3.98752558, 5.07954408],
+                                 [1.00059142, 5.65644558, 5.57669408],
+                                 [0.12252142, 5.42099558, 4.02373408],
+                                 [-1.91641858, 3.66563558, 1.67252408],
+                                 [-1.84211858, 3.26893558, 2.55711408]])
+
+    assert np.allclose(reactant.get_positions(), reactant_expected)
+    assert np.allclose(product.get_positions(), product_expected)
+
+
 def test_get_indices():
     arr = np.array(list(range(10)))
     values = [1, 5, 9]
@@ -85,12 +127,10 @@ def test_get_bond_forming_atoms(ester_hydrolysis_reaction):
 def test_get_reactive_atoms(ester_hydrolysis_reaction):
     reactant, product = ester_hydrolysis_reaction
 
-    ester = separate(reactant)[0]
-    acid = separate(product)[0]
-
     matrix = get_reactivity_matrix(reactant, product)
-    result = get_reactive_atoms(ester, acid, matrix)
-    expected = np.array([3])
+    shared = np.array([0, 1, 2, 3, 4, 5])
+    result = get_reactive_atoms(shared, matrix)
+    expected = np.array([2])
 
     assert np.all(result == expected)
 
